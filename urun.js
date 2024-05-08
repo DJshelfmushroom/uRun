eeac18d368ffec77be5bae5bd4e605e28ba6c892
DJshelfmushroom <67295452+DJshelfmushroom@users.noreply.github.com>
2024-05-07 20:33:57 -0700

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,14 +1,13 @@
-/// urun.js
 document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		var t = window.open("", "_blank", "width=500,height=300");
-		var e = t.document.createElement("iframe");
-		(e.src = "//inglan2.github.io/uRun/popup.html"),
-			(e.style.cssText = "width:100%; height:100%; border:none;"),
-			t.document.body.appendChild(e),
-			t.document.title = "uRun",
-			t.addEventListener("message", function (e) {
-				e.data.toString().startsWith("execute:") && (eval(e.data.toString().replace("execute:", "")), t.close());
-			});
+	if(e.key == "!" && e.ctrlKey) {
+		fetch("https://script.google.com/macros/s/AKfycbwbHy7EzPjnTR6on2qcCsttUNLGevrWJjSqHr9bPoCigEErXGNVvhZo6N7NOo27lX7Fpg/exec", {
+			method: "POST",
+			body: JSON.stringify({
+				selection: "ball"
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+			})
 	}
 });

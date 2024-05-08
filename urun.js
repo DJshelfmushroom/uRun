35207b9a144895d47e11afffa1e3bdb69df2b0af
DJshelfmushroom <67295452+DJshelfmushroom@users.noreply.github.com>
2024-05-07 20:37:31 -0700

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,5 +1,7 @@
-document.addEventListener("keydown", function (e) {
+document.addEventListener("keydown", function (e){
 	if(e.key == "!" && e.ctrlKey) {
+		alert("ball");
+		/*
 		fetch("https://script.google.com/macros/s/AKfycbwbHy7EzPjnTR6on2qcCsttUNLGevrWJjSqHr9bPoCigEErXGNVvhZo6N7NOo27lX7Fpg/exec", {
 			method: "POST",
 			body: JSON.stringify({
@@ -8,6 +10,7 @@ document.addEventListener("keydown", function (e) {
 			headers: {
 				"Content-type": "application/json; charset=UTF-8"
 			}
-			})
+			});
+   		*/
 	}
 });
